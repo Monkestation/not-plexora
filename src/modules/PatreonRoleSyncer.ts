8323fbb89f7e9642a55d724adcc16607def232f8
Flleeppyy <flleeppyy@gmail.com>
2026-08-04 11:24:06 -0700

    are you serious?

--- src/modules/PatreonRoleSyncer.ts
+++ src/modules/PatreonRoleSyncer.ts
@@ -110,7 +110,13 @@ export default class PatreonRoleSyncer extends BaseModule<Config> {
 			this.logger.debug("Looping through player data map");
 			for (const [ckey, playerData] of playerDataMap) {
 				this.logger.debug(`Fetching discord member data for ${ckey}`)
-				const member = await guild.members.fetch(playerData.discordLink.discord_id).catch((error) => {
+				const member = await guild.members.fetch({
+					cache: true,
+					limit: 1,
+					withPresences: false,
+					user: playerData.discordLink.discord_id,
+
+				}).catch((error) => {
 					this.logger.error(error);
 					return null;
 				});
